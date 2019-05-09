d9c322679a36ced7fd5371cf10152b687cfbf8a0
Martin Micunda <martinmicunda@hotmail.com>
2019-05-09 14:22:43 +0100

    replace functions with class

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,8 @@
  */
 import Boom from '@hapi/boom';
 import { APIGatewayEvent, Context } from 'aws-lambda';
+import { LambdaLog } from 'lambda-log';
+export declare const logger: LambdaLog;
 export interface SchedulinoAPIGatewayEvent extends APIGatewayEvent {
   principalId: string;
   cmd: string;
@@ -17,14 +19,22 @@ export interface Spec {
   validate: Validate;
   handler: () => any;
 }
-declare function handleError(error: Boom | Error): string;
-declare function respond(
-  event: SchedulinoAPIGatewayEvent,
-  context: Context,
-  spec: Spec
-): Promise<void>;
-declare function handleUnrecognizedOperation(
-  event: SchedulinoAPIGatewayEvent,
-  context: Context
-): void;
-export { respond, handleUnrecognizedOperation, handleError };
+export declare class UtilsSvc {
+  /**
+   * This is custom app error handler.
+   */
+  static handleError(error: Boom | Error): string;
+  static respond(
+    event: SchedulinoAPIGatewayEvent,
+    context: Context,
+    spec: Spec
+  ): Promise<void>;
+  static handleUnrecognizedOperation(
+    event: SchedulinoAPIGatewayEvent,
+    context: Context
+  ): void;
+  /**
+   * Validates event data with the defined validation schema.
+   */
+  private static validateInput;
+}
--- index.js
+++ index.js
@@ -11,70 +11,69 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const boom_1 = __importDefault(require("@hapi/boom"));
 const joi_1 = __importDefault(require("@hapi/joi"));
 const lambda_log_1 = require("lambda-log");
-const logger = new lambda_log_1.LambdaLog();
-// this is custom app error handler
-function handleError(error) {
-    let boomPayload;
-    if (boom_1.default.isBoom(error)) {
-        boomPayload = error.output.payload;
+exports.logger = new lambda_log_1.LambdaLog({
+    debug: process.env.LOGGER_LEVEL === 'DEBUG'
+});
+class UtilsSvc {
+    /**
+     * This is custom app error handler.
+     */
+    static handleError(error) {
+        let boomPayload;
+        if (boom_1.default.isBoom(error)) {
+            boomPayload = error.output.payload;
+        }
+        else if (error instanceof Error) {
+            try {
+                // if the error comes from another invoked-lambda we need to
+                // parse this error object however if the error was throw somewhere
+                // in the function e.g. exception it will contains string instead
+                // of JSON object so we handle this error in catch block
+                const errorObj = JSON.parse(error.message);
+                boomPayload = new boom_1.default(errorObj.message, {
+                    statusCode: errorObj.statusCode,
+                }).output.payload;
+            }
+            catch (e) {
+                exports.logger.error('APPLICATION EXCEPTION', error.stack);
+                boomPayload = boom_1.default.badImplementation(error.message).output.payload;
+            }
+        }
+        else {
+            boomPayload = boom_1.default.badImplementation().output.payload;
+        }
+        return JSON.stringify(boomPayload);
     }
-    else if (error instanceof Error) {
+    static async respond(event, context, spec) {
         try {
-            // if the error comes from another invoked-lambda we need to
-            // parse this error object however if the error was throw somewhere
-            // in the function e.g. exception it will contains string instead
-            // of JSON object so we handle this error in catch block
-            const errorObj = JSON.parse(error.message);
-            boomPayload = new boom_1.default(errorObj.message, {
-                statusCode: errorObj.statusCode,
-            }).output.payload;
+            if (spec.validate) {
+                UtilsSvc.validateInput(event, spec.validate);
+            }
+            const data = await spec.handler();
+            context.succeed(data);
         }
-        catch (e) {
-            logger.error('APPLICATION EXCEPTION', error.stack);
-            boomPayload = boom_1.default.badImplementation(error.message).output.payload;
+        catch (error) {
+            context.fail(UtilsSvc.handleError(error));
         }
     }
-    else {
-        boomPayload = boom_1.default.badImplementation().output.payload;
+    static handleUnrecognizedOperation(event, context) {
+        context.fail(UtilsSvc.handleError(boom_1.default.badRequest(`Unrecognized action command ${event.cmd}`)));
     }
-    return JSON.stringify(boomPayload);
-}
-exports.handleError = handleError;
-/**
- * Validates event data with the defined validation schema.
- *
- * @param {event} event - lambda event data
- * @param {Object} validate - validation schema
- * @throws {Error} Will throw an error if it failed to validate schema.
- * @api private
- */
-function validateInput(event, validate) {
-    const props = Object.keys(validate);
-    for (let i = 0; i < props.length; i += 1) {
-        const prop = props[i];
-        // FIXME: what type is error
-        // tslint:disable-next-line:no-any
-        const error = joi_1.default.validate(event[prop], validate[prop]) // tslint:disable-next-line:no-any
-            .error;
-        if (error) {
-            throw boom_1.default.badRequest(error);
+    /**
+     * Validates event data with the defined validation schema.
+     */
+    static validateInput(event, validate) {
+        const props = Object.keys(validate);
+        for (let i = 0; i < props.length; i += 1) {
+            const prop = props[i];
+            // FIXME: what type is error
+            // tslint:disable-next-line:no-any
+            const error = joi_1.default.validate(event[prop], validate[prop]) // tslint:disable-next-line:no-any
+                .error;
+            if (error) {
+                throw boom_1.default.badRequest(error);
+            }
         }
     }
 }
-async function respond(event, context, spec) {
-    try {
-        if (spec.validate) {
-            validateInput(event, spec.validate);
-        }
-        const data = await spec.handler();
-        context.succeed(data);
-    }
-    catch (error) {
-        context.fail(handleError(error));
-    }
-}
-exports.respond = respond;
-function handleUnrecognizedOperation(event, context) {
-    context.fail(handleError(boom_1.default.badRequest(`Unrecognized action command ${event.cmd}`)));
-}
-exports.handleUnrecognizedOperation = handleUnrecognizedOperation;
+exports.UtilsSvc = UtilsSvc;
