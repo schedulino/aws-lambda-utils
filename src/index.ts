a75a4dd042b4a000ed36810445d3a1d5e3eee071
Martin Micunda <martinmicunda@hotmail.com>
2019-05-15 17:53:26 +0100

    feat(utils): add response builder function to handle api gateway proxy event

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,11 @@ export { default as Boom } from '@hapi/boom';
 export const logger = new LambdaLog({
   debug: process.env.LOGGER_LEVEL === 'DEBUG',
 });
-//
-// export interface SchedulinoAPIGatewayEvent extends APIGatewayEvent {
-//   principalId: string;
-//   cmd: string;
-// }
+
+export interface SchedulinoAPIGatewayEvent extends APIGatewayEvent {
+  principalId: string;
+  cmd: string;
+}
 
 const headers = {
   'Access-Control-Allow-Origin': '*', // This is required to make CORS work with AWS API Gateway Proxy Integration.
